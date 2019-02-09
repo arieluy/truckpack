e24184a8bca81486b3e807de809196e67ae27e0c
Ariel Uy <auy@andrew.cmu.edu>
2019-02-09 03:03:39 -0500

    Update item method

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Stage, Layer, Rect, Text, Group } from 'react-konva';
+import { Stage, Layer, Rect, Text } from 'react-konva';
 import Konva from 'konva';
 import Item from './Item';
 import Truck from './Truck';
@@ -56,7 +56,7 @@ class Item_d extends React.Component {
         x={item.x}
         y={item.y}
       />
-      <> 
+      </> 
     );
   }
 }
@@ -67,19 +67,18 @@ class App extends Component {
     truckWidth: 0,
     truckLength: 0,
     items: []
-  };
+  }; 
 
-  _ updateItems=(i, newItem)=> {
-    newState = [...this.state.items];
+  updateItems=(i, newItem)=> {
+    const newState = [...this.state.items];
     newState[i] = newItem;
     this.setState({items: newState})
-  }
+  };
 
   render() {
     const items = [];
     const truck = new Truck(2*240,2*96,2*84,items);
     const item = new Item(100,100,100,10,10,10,"item",true);
-    console.log("egg", item);
     items.push(item);
     const itemComponents = items.map(it=> <Item_d item={it}/>);
     console.log("Hello", itemComponents)
